61c1a415af7c8353447b99fc0c818af7d02fc07a
shagunmer08-creator <shagunmer08@gmail.com>
2025-09-07 22:14:09 +0530

    Add files via upload

---
+++ expt2shagun.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+int main(){
+    printf("shagun mer\n");
+    printf("near microwave station\n");
+    printf("tallital\n");
+    printf("bhimtal\n");
+    printf("nainital,uttarakhand\n");
+    return 0;
+}
