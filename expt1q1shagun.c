3ffa0f5d359472cec023df46e9dfe7a825f6c931
shagunmer08-creator <shagunmer08@gmail.com>
2025-09-07 22:16:53 +0530

    Add files via upload

---
+++ expt1q1shagun.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+int main(){
+    printf("shagun mer\n");
+
+    int num1,num2,sum;
+    printf("enter the first no.:");
+    scanf("%d",&num1);
+
+    printf("enter the second no.:");
+    scanf("%d",&num2);
+    
+    sum = num1 + num2;
+    printf("sum of %d and %d is %d\n", num1, num2, sum);
+    
+    return 0;
+}
+
+
