057e288194c83bc3c467466bbdff6531d328903e
shagunmer08-creator <shagunmer08@gmail.com>
2025-09-07 22:09:09 +0530

    Delete expt2 shagun.c

--- expt2 shagun.c
+++
@@ -1,9 +0,0 @@
-#include <stdio.h>
-int main(){
-    printf("shagun mer\n");
-    printf("near microwave station\n");
-    printf("tallital\n");
-    printf("bhimtal\n");
-    printf("nainital,uttarakhand\n");
-    return 0;
-}
